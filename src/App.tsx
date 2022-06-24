3bc85c7109372646582de7d23f1302d1a06beff2
Craig Miller <craigmiller160@gmail.com>
2022-06-24 14:01:04 -0400

    Integrated AuthenticationProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Navbar } from './components/Navbar';
+import { AuthenticationProvider } from './components/Authentication';
 
 function App() {
 	return (
-		<>
+		<AuthenticationProvider>
 			<CssBaseline />
 			<Navbar />
 			<div className="App">
 				<h1>Hello World</h1>
 			</div>
-		</>
+		</AuthenticationProvider>
 	);
 }
 
