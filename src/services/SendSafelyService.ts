8f7395e6f21ac0d9cbf8834c19fba54e97e5b4b7
Craig Miller <craigmiller160@gmail.com>
2022-06-24 16:39:50 -0400

    Working on base request

--- src/services/SendSafelyService.ts
+++ src/services/SendSafelyService.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestHeaders } from 'axios';
 import {
 	BaseAuthResponse,
 	ResponseType,
@@ -12,10 +12,12 @@ import { Authentication } from '../components/Authentication';
 const REQUEST_KEY_HEADER = 'ss-api-key';
 const REQUEST_TIMESTAMP_HEADER = 'ss-request-timestamp';
 const REQUEST_SIGNATURE_HEADER = 'ss-request-signature';
+const BASE_URL = 'https://app.sendsafely.com';
+type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 const sendSafelyApi = axios.create({
 	// baseURL: 'https://demo.sendsafely.com/api/v2.0'
-	baseURL: 'https://app.sendsafely.com'
+	baseURL: BASE_URL
 });
 
 export interface AuthenticateParams {
@@ -40,6 +42,54 @@ export const authenticate = ({
 			}
 			throw new Error(JSON.stringify(data));
 		});
+
+const baseSendSafelyRequest = <T>(
+	authentication: Authentication,
+	uri: string,
+	method: Method,
+	body: any // TODO what about GET?
+): Promise<T> => {
+	const fullUrl = `${BASE_URL}${uri}`;
+	const timestamp = generateRequestTimestamp();
+	const bodyAsString = JSON.stringify(body);
+	const signature = generateRequestSignature(
+		authentication,
+		fullUrl,
+		timestamp,
+		bodyAsString
+	);
+	const headers: AxiosRequestHeaders = {
+		[REQUEST_KEY_HEADER]: authentication.apiKey!!,
+		[REQUEST_TIMESTAMP_HEADER]: timestamp,
+		[REQUEST_SIGNATURE_HEADER]: signature
+	};
+	switch (method) {
+		case 'GET':
+			return sendSafelyApi
+				.get<T>(uri, {
+					headers
+				})
+				.then((res) => res.data);
+		case 'POST':
+			return sendSafelyApi
+				.post<T>(uri, body, {
+					headers
+				})
+				.then((res) => res.data);
+		case 'PUT':
+			return sendSafelyApi
+				.put<T>(uri, body, {
+					headers
+				})
+				.then((res) => res.data);
+		case 'DELETE':
+			return sendSafelyApi
+				.delete<T>(uri, {
+					headers
+				})
+				.then((res) => res.data);
+	}
+};
 
 export const generateRequestTimestamp = (): string =>
 	format(new Date(), "yyyy-MM-dd'T'HH:mm:ssZZZZ");
