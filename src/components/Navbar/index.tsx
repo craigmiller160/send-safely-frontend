836d2b9091cf3af2ce7ec84152ac98a0ec9b8bf9
Craig Miller <craigmiller160@gmail.com>
2022-06-25 15:48:44 -0400

    Got switch style working

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,34 @@
-import { AppBar, Box, Switch, Toolbar, Typography } from '@mui/material';
+import {
+	AppBar,
+	Box,
+	FormControlLabel,
+	Switch,
+	Toolbar,
+	Typography
+} from '@mui/material';
+import { useIsAuthenticated } from '../Authentication';
 
-export const Navbar = () => (
-	<Box sx={{ flexGrow: 1 }}>
-		<AppBar position="static">
-			<Toolbar>
-				<Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-					Send Safely Frontend Assessment
-				</Typography>
-				<Switch />
-			</Toolbar>
-		</AppBar>
-	</Box>
-);
+export const Navbar = () => {
+	const isAuthenticated = useIsAuthenticated();
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography
+						variant="h5"
+						component="div"
+						sx={{ flexGrow: 1 }}
+					>
+						Send Safely Frontend Assessment
+					</Typography>
+					{isAuthenticated && (
+						<FormControlLabel
+							control={<Switch color="secondary" />}
+							label="Dummy Data"
+						/>
+					)}
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+};
