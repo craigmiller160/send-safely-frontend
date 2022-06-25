4766707f9a10755e4a4ade5f3ad29731c34a51c3
Craig Miller <craigmiller160@gmail.com>
2022-06-25 11:23:07 -0400

    Tweaking the query behavior

--- src/components/Content/Packages/index.tsx
+++ src/components/Content/Packages/index.tsx
@@ -9,7 +9,12 @@ export const Packages = () => {
 	const { data, error, isLoading } = useQuery<
 		SendSafelyPackageResponse,
 		Error
-	>('sentPackages', () => SendSafelyService.getSentPackages(authentication));
+	>('sentPackages', () => SendSafelyService.getSentPackages(authentication), {
+		// TODO consider if I really want to disable these
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		refetchOnReconnect: false
+	});
 	return (
 		<>
 			{isLoading ? 'Loading...' : ''}
