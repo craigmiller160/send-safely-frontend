785660c539f938696714498288f66775c226d3bc
Craig Miller <craigmiller160@gmail.com>
2022-06-25 15:40:29 -0400

    Stubbed out dummy service methods

--- src/services/DummyDataService.ts
+++ src/services/DummyDataService.ts
@@ -1,1 +1,24 @@
+import { Authentication } from '../components/Authentication';
+import {
+	SendSafelyReceivedPackage,
+	SendSafelyReceivedPackageResponse,
+	SendSafelySentPackageResponse
+} from '../types/sendSafely';
+
 export {};
+
+const PAGE_SIZE = 10;
+
+export const getReceivedPackages = (
+	authentication: Authentication,
+	pageNumber: number = 0
+): Promise<SendSafelyReceivedPackageResponse> => {
+	// TODO finish this
+};
+
+export const getSentPackages = (
+	authentication: Authentication,
+	pageNumber: number = 0
+): Promise<SendSafelySentPackageResponse> => {
+	// TODO finish this
+};
