b5907919bc40255fffae0203092a47fc62908402
Craig Miller <craigmiller160@gmail.com>
2022-06-25 15:46:17 -0400

    Got dummy data generating functions working

--- src/services/DummyDataService.ts
+++ src/services/DummyDataService.ts
@@ -2,6 +2,8 @@ import { Authentication } from '../components/Authentication';
 import {
 	SendSafelyReceivedPackage,
 	SendSafelyReceivedPackageResponse,
+	SendSafelyResponseType,
+	SendSafelySentPackage,
 	SendSafelySentPackageResponse
 } from '../types/sendSafely';
 
@@ -13,12 +15,35 @@ export const getReceivedPackages = (
 	authentication: Authentication,
 	pageNumber: number = 0
 ): Promise<SendSafelyReceivedPackageResponse> => {
-	// TODO finish this
+	const packages = Array.from(new Array(PAGE_SIZE).keys()).map(
+		(index): SendSafelyReceivedPackage => ({
+			packageId: `Package-Id-${pageNumber}${index}`,
+			packageUserName: `Sender-${pageNumber}${index}`,
+			packageUpdateTimestamp: `Timestamp-${pageNumber}${index}`,
+			filenames: [`Filename-${pageNumber}${index}`]
+		})
+	);
+	return Promise.resolve({
+		response: SendSafelyResponseType.SUCCESS,
+		packages
+	});
 };
 
 export const getSentPackages = (
 	authentication: Authentication,
 	pageNumber: number = 0
 ): Promise<SendSafelySentPackageResponse> => {
-	// TODO finish this
+	const packages = Array.from(new Array(PAGE_SIZE).keys()).map(
+		(index): SendSafelySentPackage => ({
+			packageId: `Package-Id-${pageNumber}${index}`,
+			packageUserName: `Sender-${pageNumber}${index}`,
+			packageUpdateTimestamp: `Timestamp-${pageNumber}${index}`,
+			filenames: [`Filename-${pageNumber}${index}`],
+			recipients: [`Recipient-${pageNumber}${index}`]
+		})
+	);
+	return Promise.resolve({
+		response: SendSafelyResponseType.SUCCESS,
+		packages
+	});
 };
