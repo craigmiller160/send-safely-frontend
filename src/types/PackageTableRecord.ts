0aa26707448247b8aea79df36027cbe22e4d5901
Craig Miller <craigmiller160@gmail.com>
2022-06-25 11:51:56 -0400

    Deleted unnecessary file

--- src/types/PackageTableRecord.ts
+++
@@ -1,7 +0,0 @@
-export interface PackageTableRecord {
-	readonly packageId: string;
-	readonly sender: string;
-	readonly timestamp: string;
-	readonly recipients: string;
-	readonly filenames: string;
-}
