7fbcf16a3757ad9f63e784599c5ce0b611f18a25
Craig Miller <craigmiller160@gmail.com>
2022-06-25 11:06:21 -0400

    Refactored SendSafely types

---
+++ src/types/sendSafely/index.d.ts
@@ -0,0 +1,39 @@
+import { Package } from './PackagesResponse';
+
+export enum SendSafelyResponseType {
+	SUCCESS = 'SUCCESS',
+	AUTHENTICATION_FAILED = 'AUTHENTICATION_FAILED'
+}
+
+export interface SendSafelyBaseResponse {
+	readonly response: SendSafelyResponseType;
+}
+
+export interface SendSafelySuccessResponse extends SendSafelyBaseResponse {
+	readonly response: SendSafelyResponseType.SUCCESS;
+}
+
+export interface SendSafelyAuthResponse extends SendSafelySuccessResponse {
+	readonly apiKey: string;
+	readonly apiSecret: string;
+	readonly email: string;
+}
+
+export interface SendSafelyErrorResponse extends SendSafelyBaseResponse {
+	readonly response: Exclude<
+		SendSafelyResponseType,
+		SendSafelyResponseType.SUCCESS
+	>;
+	readonly message: string;
+}
+
+export interface SendSafelyPackage {
+	readonly packageId: string;
+	readonly packageUpdateTimestamp: string;
+	readonly recipients: ReadonlyArray<string>;
+	readonly filenames: ReadonlyArray<string>;
+}
+
+export interface SendSafelyPackageResponse extends SendSafelySuccessResponse {
+	readonly packages: ReadonlyArray<Package>;
+}
